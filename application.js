422878f6f27dfa30d83ca7064ace70a3a0345a8e
Adrian_F <adrian.frimpong@gmail.com>
2013-11-08 11:43:25 -0600

    Updated javascript lol

--- application.js
+++ application.js
@@ -2,25 +2,33 @@ $(function () {
    var answer = Math.floor(101*Math.random());
    var wrong = true;
    var guess, prev_guess;
-   while(wrong) {
-    guess = Number(prompt("Guess a number from 1 to 100.","1..10...100"));
+   
+   var game = function(){
 
-        if (guess==answer) {
-            alert("Congrats! You win.");
-            wrong=false;
-        } else if(prev_guess!= null) {
-            if (Math.abs(answer-guess) > Math.abs(answer - prev_guess)) {
-                alert("You're getting warmer!");
-            }else {
-                alert("You're getting colder!");
+        while(wrong){
+            guess = Number(prompt("Guess a number from 1 to 100.","1..10...100"));
+
+            if (guess==answer) {
+                alert("Congrats! You win.");
+                wrong=false;
+
+            } else if(prev_guess!= null) {
+                
+                if (Math.abs(answer-guess) < Math.abs(answer - prev_guess)) {
+                    alert("You're getting warmer!");
+                }else {
+                    alert("You're getting colder!");
+                }
             }
-        }
-        else if(guess>answer) {
-            alert("Too high!");
-        } else {
-            alert("Too low!");
-        }
+            
+            else if(guess>answer) {
+                alert("Too high!");
+            } else {
+                alert("Too low!");
+            }
 
-        prev_guess = guess;
-   }
+            prev_guess = guess;
+        }
+    };
+   game();
 });
