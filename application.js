643526871d26758ae85cb0df8d0c09938e71d50d
Adrian_F <adrian.frimpong@gmail.com>
2013-11-09 19:28:46 -0600

    Workable proj

--- application.js
+++ application.js
@@ -1,37 +1,47 @@
 $(function () {
-   var answer = Math.floor(101*Math.random());
-   var wrong = true;
-   var guess, prev_guess;
    
-   var game = function(){
+   var answer = Math.floor(101*Math.random());
+   console.log("Answer is " + answer);
+   var wrong = true;
+   var prev_guess, msg, player_guess;
+      
+   console.log(player_guess);
 
-        while(wrong){
-            guess = Number(prompt("Guess a number from 1 to 100.","1..10...100"));
+   var game = function(guess){
 
-            if (guess==answer) {
-                alert("Congrats! You win.");
-                wrong=false;
+        if (guess==answer) {
+            msg = "Congrats! You win.";
+            wrong=false;
 
-            } else if (guess == prev_guess) {
-                alert("You just guessed that number..");
-            }
-            else if(prev_guess!= null) {
-                
-                if (Math.abs(answer-guess) < Math.abs(answer - prev_guess)) {
-                    alert("You're getting warmer!");
-                }else {
-                    alert("You're getting colder!");
-                }
-            }
-
-            else if(guess>answer) {
-                alert("Too high!");
-            } else {
-                alert("Too low!");
-            }
-
-            prev_guess = guess;
+        } else if (guess == prev_guess) {
+            msg = "You just guessed that number..";
         }
+        else if(prev_guess!= null) {
+            
+            if (Math.abs(answer-guess) < Math.abs(answer - prev_guess)) {
+                msg = "You're getting warmer!";
+            }else {
+                msg = "You're getting colder!";
+            }
+        }
+
+        else if(guess>answer) {
+            msg = "Too high!";
+        } else {
+            msg = "Too low!";
+        }
+
+        prev_guess = guess;
+        return msg;
     };
    // game();
+   /*create event on hitting enter or submitting a guess*/
+    $('#enterGuess').click(function(){
+        // alert('handler for .click() called.');
+        player_guess =  Number($('#guess').val());
+        $('#output').html(game(player_guess));
+        console.log(player_guess);
+        return false;
+    });
+
 });
